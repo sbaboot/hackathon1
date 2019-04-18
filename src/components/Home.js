8dc80a04625469d92fe64a1fb29f485cbaaaf997
Agathe Cazals <agathe.cazals@gmail.com>
2019-04-18 17:03:21 +0200

    Page accueil v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
+import {
+  Container, Row, Col, Button
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: {}
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("http://easteregg.wildcodeschool.fr/api/characters/random", {
+      crossDomain: true
+    })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          characters: data
+        });
+      });
+  }
+
   render() {
+    const { characters } = this.state;
     return (
-      <div className="main">
-        <img src="https://cdn.pixabay.com/photo/2019/03/30/23/24/away-4092262_960_720.jpg" alt="paysage de fond" />
+      <div className="header">
+        <div className="borderTitle">
+          <div className="headerOverlay">
+            <h1 className="headerTitle">Chasse aux oeufs</h1>
+            <h5 className="headerText">Repondez juste au différentes questions pour obtenir le plus d'oeuf, et peut-être ... l'oeuf cosmique !</h5>
+            <Row>
+              <Col className="col-lg-12 projet-titre">
+                <h2 className="orange-color">Aujourd'hui je joue avec :</h2>
+              </Col>
+            </Row>
+            <Container>
+              <div className="title col-md-12 col-sm-12 col-12 p-4 name">
+                <h2>{characters.name}</h2>
+              </div>
+              <br />
+              <Row>
+                <Col>
+                  <div className="item margin-description">
+                    <img className="charactersImg" src={characters.image} alt={characters.name} /> </div>
+                </Col>
+              </Row>
+            </Container>
+            <Container>
+              <Button className="btnTryAgain" color="warning" onClick={this.componentDidMount}>Try Again</Button>
+            </Container>
+            <Container>
+              <Button className="btnLetsGo" color="warning">Let's go !!</Button>
+            </Container>
+          </div>
+        </div>
       </div>
+
     );
   }
 }

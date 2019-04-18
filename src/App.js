4f9d126d2248809f8dec10218968e045a1cb301f
Christine Rua <christine2704@outlook.fr>
2019-04-18 16:08:10 +0200

    base quiz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import QuizApp from './components/QuizApp';
 import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <QuizApp totalQuestions={10} />
       </div>
     );
   }

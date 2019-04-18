9fdf41770252726808cf367bc8615a71ba78d01f
Christine Rua <christine2704@outlook.fr>
2019-04-18 15:32:02 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Memory from './components/Memory';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Switch>
-       {/*  <Route path="/End" component={End} />
-         <Route path="/Enigme" component={Enigme} />
-        <Route path="/Pacman" component={Pacman} /> 
-        <Route path="/Memory" component={Memory} /> */}
-        <Route path="/" component={Home} />
-      </Switch>
+
       </div>
     );
   }
